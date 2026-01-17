75f2338204d5c0f926614afd9264b409d517d27e
Pratik <pratikpatil12324@gmail.com>
2026-01-17 20:41:24 +0530

    Lecture Activity added

--- JavaScript/LEC/Comparision.js
+++ JavaScript/LEC/Comparision.js
@@ -5,11 +5,14 @@
 // console.log(2==1);
 // console.log(2!=1);
 
-console.log("2">1);
-console.log("02">1);//o/p is unpredictable don't use this
-console.log(null>0);
-console.log(null == 0);
-console.log(null>=0);
+//null converts 0 only when there is comparision like <,>,<=,>= but not in the equality 
+console.log("2">1); //true
+console.log("02">1);//o/p is unpredictable don't use this //true
+console.log(null>0);//false
+console.log(null == 0); //false
+console.log(null>=0);//true
+console.log(null==null);//true
+console.log(null==undefined);//true
 
 
 console.log(undefined==0);
--- JavaScript/LEC/conversionOperation.js
+++ JavaScript/LEC/conversionOperation.js
@@ -10,21 +10,66 @@ let scoreundef=undefined;//decleared but can't initialized
 // console.log(typeof(scorenull));
 // console.log(typeof(scoreundef));
 
-//type convertion
-let p ="02abc";
-let pconverts=Number(p);
-console.log(typeof(p));
-console.log(pconverts)//if in the string there is number it will converts but there is not a number in the string then it will gives NaN;
-console.log(typeof(pconverts));
+// //type convertion
+// let p ="02abc";
+// let pconverts=Number(p);
+// //IMP:if there is non numeric number inside string it will throws NaN;
+// console.log(typeof(p));
+// console.log(pconverts)//if in the string there is number it will converts but there is not a number in the string then it will gives NaN;
+// console.log(typeof(pconverts));
 
-let pconvertsString=String(pconverts);
+// let pconvertsString=String(pconverts);
 
-console.log(typeof(pconvertsString));
-
-//Activity No:1--all convertios
+// console.log(typeof(pconvertsString));
 
 
-//Operations
+//-----------------------------------------Activity No:01-----------------------------------------
+
+//Activity No:1--all convertions
+let str="123";
+
+//1. string to number
+let strNumber = Number(str);
+console.log(`type of strNumber :${typeof (strNumber)}`);
+console.log(strNumber);
+
+//2.str to boolean & boolean to number 
+let strboolean = Boolean(str);
+let strboolNumber=Number(strboolean);
+console.log(strboolNumber);
+console.log(`type of strboolean :${typeof (strboolean)}`);
+console.log(strboolean);
+
+
+
+let strnull = null;
+console.log(`type of strnull :${typeof (strnull)}`);
+console.log(strnull);
+
+//3.Null
+console.log(Number(null)); //0
+console.log(String(null)); //"null"
+console.log(Boolean(null)); //false
+
+//4.undefined
+console.log(Number(undefined)); //NaN
+console.log(String(undefined)); //"undefined"
+console.log(Boolean(undefined)); //false
+
+//5.bigInt
+console.log(Number(10n)); // 10
+console.log(String(10n)); //"10"
+console.log(Boolean(10n)); //true
+
+
+
+
+
+
+//-----------------------------------------Operations-------------------------------------
+
+
+// 
 
 //Arithmatic operations
 console.log(2+2);
@@ -41,4 +86,5 @@ console.log("1"+2);
 console.log("1"+2+2);
 console.log(1+"2");
 console.log(1+3+"2");
+console.log("5"-2); //force to numeric convertion
 console.log((3+4)*5%3);
